c3ab6c0f9a35ea9caa17c4b14dc00e138ecc4b2e
cam17380 <cam17380@gmail.com>
2022-02-14 16:07:15 +0900

    Initial Commit

---
+++ Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by systembank on 2022/02/14.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
