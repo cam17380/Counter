87ce28eece32a328030a9a8e4dcaada6f4a5dafe
cam17380 <cam17380@gmail.com>
2022-02-14 16:22:40 +0900

    Step2 Add object.

--- Counter/ContentView.swift
+++ Counter/ContentView.swift
@@ -9,7 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Spacer()
+            
+            Text("1234")
+                .font(.largeTitle)
+                .frame(width: 150.0, height: 50.0)
+                .border(Color.blue)
+            
+            Spacer()
+            
+            Button("+1") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            .frame(width: 70.0, height: 50.0)
+            .foregroundColor(Color.black)
+            .background(Color.blue)
+            .cornerRadius(24)
+            
+            Spacer()
+            
+            Button("リセット") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            .frame(width: 150.0, height: 50.0)
+            .foregroundColor(Color.black)
+            .background(Color.orange)
+            .cornerRadius(24)
+            
+            Spacer()
+        }
     }
 }
 
